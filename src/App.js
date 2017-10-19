8049c6aa6b8ce5936f05503ff56d1c7f25be4b4d
Mateusz Kwiecien <kwiecienmate@gmail.com>
2017-10-19 16:49:14 +0200

    button added

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { color: 'gray'};
+    }
+
+    onClick = () => {
+        const color = this.state.color === 'gray' ? 'violet' : 'gray';
+        this.setState({ color });
+    }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className={"App " + this.state.color}>
+        <button onClick={this.onClick}>push me!</button>
       </div>
     );
   }
